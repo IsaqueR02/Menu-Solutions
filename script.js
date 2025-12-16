d11ba2c7b432bbfcaeffaa9295a95b32dea689ef
Isaque Roberto <isaquezulato@gmail.com>
2025-12-16 15:49:52 -0300

    add exercise 01

--- script.js
+++ script.js
@@ -35,3 +35,21 @@ function executarExercicio() {
             console.log('Exercício não encontrado.');
     }
 }
+
+function exercicio1() {
+    console.log('Executando Exercício 1: Verificador de Número Positivo, Negativo ou Zero');
+            
+            // Conversão da entrada para o número com parseFloat()
+            const input = prompt("1. Digite um número para verificar se é positivo, negativo ou zero:");
+            const num = parseFloat(input);
+
+            if (isNaN(num)) {
+                console.log("Erro: Entrada inválida. Por favor, digite um número.");
+            } else if (num > 0) {
+                console.log(`O número ${num} é positivo.`);
+            } else if (num < 0) {
+                console.log(`O número ${num} é negativo.`);
+            } else { // num é igual a 0
+                console.log(`O número é zero.`);
+            }
+}
